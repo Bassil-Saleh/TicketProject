e61d7693553b4a083b9e78895511701b0b25faf5
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-02 19:52:24 -0400

    Add a query to find a Session entity by its token hash

--- src/main/java/com/ticketproject/webapp/model/repositories/SessionRepository.java
+++ src/main/java/com/ticketproject/webapp/model/repositories/SessionRepository.java
@@ -3,7 +3,11 @@ package com.ticketproject.webapp.model.repositories;
 import com.ticketproject.webapp.model.entities.Session;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * SessionRepository is used to perform CRUD operations with Session entities.
@@ -11,4 +15,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long>
 {
+    /**
+     * Find a Session entity by its token hash.
+     * @param hash the token hash to search for
+     * @return an Optional containing the Session if found, empty otherwise
+     */
+    @Query("SELECT s FROM Session s WHERE s.tokenHash = :hash")
+    Optional<Session> findByTokenHash(@Param("hash") byte[] hash);
 }
