52754ab7b984699a52761deb682b77137b5e279a
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-05 20:56:29 -0400

    Use named parameters for LogoutEventHost.sh

--- api-scripts/model/Session/LogoutEventHost.sh
+++ api-scripts/model/Session/LogoutEventHost.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "Need to supply a JSON Web Token"
-	exit 1;
+# Defaults
+token=""
+
+# Parse named parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--token) token="$2"; shift 2 ;;
+		*)       shift ;;
+	esac
+done
+
+# Validate mandatory parameters
+if [ -z "$token" ]; then
+	echo "Usage: $0 --token <value>"
+	exit 1
 fi
 
-header="Authorization: Bearer $1"
-#echo $header
+header="Authorization: Bearer $token"
 
 curl -X PATCH -H "$header" http://localhost:8080/api/v1/sessions/logout
+echo
