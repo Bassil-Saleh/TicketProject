e7a4ca1b18a0af8846407720c6b10863f73421df
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-05 20:24:12 -0400

    Use named parameters for EditEventHostEmail.sh

--- api-scripts/model/EventHost/EditEventHostEmail.sh
+++ api-scripts/model/EventHost/EditEventHostEmail.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "Need to supply a JSON Web Token"
-	exit 1;
+# Defaults
+token=""
+email=""
+
+# Parse named parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--token) token="$2"; shift 2 ;;
+		--email) email="$2"; shift 2 ;;
+		*)       shift ;;
+	esac
+done
+
+# Validate mandatory parameters
+if [ -z "$token" ] || [ -z "$email" ]; then
+	echo "Usage: $0 --token <value> --email <value>"
+	exit 1
 fi
 
-if [ -z "$2" ]; then
-	echo "Need to supply the new email address"
-	exit 1;
-fi
-
-authHeader="Authorization: Bearer $1"
+authHeader="Authorization: Bearer $token"
 contentTypeHeader="Content-Type: application/json"
-json="{\"email\": \"$2\"}"
+json="{\"email\": \"$email\"}"
 
 curl -X PATCH -H "$authHeader" -H "$contentTypeHeader" -d "$json" http://localhost:8080/api/v1/event-hosts/email
+echo
