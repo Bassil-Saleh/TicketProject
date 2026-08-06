364f7da078b823d2d0dbd8817617825f839a6e68
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-05 20:31:19 -0400

    Use named parameters for EditEventHostPassword.sh

--- api-scripts/model/EventHost/EditEventHostPassword.sh
+++ api-scripts/model/EventHost/EditEventHostPassword.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "Need to supply a JSON Web Token"
-	exit 1;
+# Defaults
+token=""
+password=""
+
+# Parse named parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--token)    token="$2";    shift 2 ;;
+		--password) password="$2"; shift 2 ;;
+		*)          shift ;;
+	esac
+done
+
+# Validate mandatory parameters
+if [ -z "$token" ] || [ -z "$password" ]; then
+	echo "Usage: $0 --token <value> --password <value>"
+	exit 1
 fi
 
-if [ -z "$2" ]; then
-	echo "Need to supply the new password"
-	exit 1;
-fi
-
-authHeader="Authorization: Bearer $1"
+authHeader="Authorization: Bearer $token"
 contentTypeHeader="Content-Type: application/json"
-json="{\"password\": \"$2\"}"
+json="{\"password\": \"$password\"}"
 
 curl -X PATCH -H "$authHeader" -H "$contentTypeHeader" -d "$json" http://localhost:8080/api/v1/event-hosts/password
+echo
