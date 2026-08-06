3fe04f5c0a9100b53c177c491752a9d8e7987a10
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-05 20:48:23 -0400

    Use named parameters for CreatePasswordResetToken.sh

--- api-scripts/model/PasswordResetToken/CreatePasswordResetToken.sh
+++ api-scripts/model/PasswordResetToken/CreatePasswordResetToken.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "Need to supply an email address"
-	exit 1;
+# Defaults
+email=""
+
+# Parse named parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--email) email="$2"; shift 2 ;;
+		*)       shift ;;
+	esac
+done
+
+# Validate mandatory parameters
+if [ -z "$email" ]; then
+	echo "Usage: $0 --email <value>"
+	exit 1
 fi
 
-json="{\"email\": \"$1\"}"
+json="{\"email\": \"$email\"}"
 
 curl --json "$json" http://localhost:8080/api/v1/password-reset-tokens
+echo
