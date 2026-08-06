366a600aa4bad13b54da675acd5bd384fd22cefb
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-05 20:53:28 -0400

    Use named parameters for LoginEventHost.sh

--- api-scripts/model/Session/LoginEventHost.sh
+++ api-scripts/model/Session/LoginEventHost.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "Need to supply an email address"
-	exit 1;
+# Defaults
+email=""
+password=""
+
+# Parse named parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--email)    email="$2";    shift 2 ;;
+		--password) password="$2"; shift 2 ;;
+		*)          shift ;;
+	esac
+done
+
+# Validate mandatory parameters
+if [ -z "$email" ] || [ -z "$password" ]; then
+	echo "Usage: $0 --email <value> --password <value>"
+	exit 1
 fi
 
-if [ -z "$2" ]; then
-	echo "Need to supply a password"
-	exit 1;
-fi
-
-json="{\"email\": \"$1\", \"password\": \"$2\"}"
-#echo $json
+json="{\"email\": \"$email\", \"password\": \"$password\"}"
 
 curl --json "$json" http://localhost:8080/api/v1/sessions/login
+echo
