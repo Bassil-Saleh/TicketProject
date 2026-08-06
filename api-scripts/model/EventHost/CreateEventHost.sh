7ee4c06ca20b875f94101fdfbac2e66a6266e1d2
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-05 20:18:37 -0400

    Use named parameters for CreateEventHost.sh

--- api-scripts/model/EventHost/CreateEventHost.sh
+++ api-scripts/model/EventHost/CreateEventHost.sh
@@ -1,42 +1,41 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "Need to supply a first name"
-	exit 1;
+# Defaults
+firstName=""
+middleName=""
+lastName=""
+email=""
+password=""
+dateOfBirth=""
+
+# Parse named parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--first-name)    firstName="$2";    shift 2 ;;
+		--middle-name)   middleName="$2";   shift 2 ;;
+		--last-name)     lastName="$2";     shift 2 ;;
+		--email)         email="$2";        shift 2 ;;
+		--password)      password="$2";     shift 2 ;;
+		--date-of-birth) dateOfBirth="$2";  shift 2 ;;
+		*)               shift ;;
+	esac
+done
+
+# Validate mandatory parameters
+if [ -z "$firstName" ] || [ -z "$lastName" ] || [ -z "$email" ] || [ -z "$password" ] || [ -z "$dateOfBirth" ]; then
+	echo "Usage: $0 --first-name <value> --last-name <value> --email <value> --password <value> --date-of-birth <value> [--middle-name <value>]"
+	exit 1
 fi
 
-if [ -z "$2" ]; then
-	echo "Need to supply a middle name"
-	exit 1;
+# Build JSON (only include middleName if provided)
+json="{\"firstName\": \"$firstName\""
+if [ -n "$middleName" ]; then
+	json="${json}, \"middleName\": \"$middleName\""
 fi
-
-if [ -z "$3" ]; then
-	echo "Need to supply a last name"
-	exit 1;
-fi
-
-if [ -z "$4" ]; then
-	echo "Need to supply an email address"
-	exit 1;
-fi
-
-if [ -z "$5" ]; then
-	echo "Need to supply a password"
-	exit 1;
-fi
-
-if [ -z "$6" ]; then
-	echo "Need to supply a date of birth"
-	exit 1;
-fi
-
-json="{\"firstName\": \"$1\","
-json="${json} \"middleName\": \"$2\","
-json="${json} \"lastName\": \"$3\","
-json="${json} \"email\":\"$4\","
-json="${json} \"password\":\"$5\","
-json="${json} \"dateOfBirth\":\"$6\"}"
-
-#echo "$json"
+json="${json}, \"lastName\": \"$lastName\""
+json="${json}, \"email\": \"$email\""
+json="${json}, \"password\": \"$password\""
+json="${json}, \"dateOfBirth\": \"$dateOfBirth\"}"
 
 curl --json "$json" http://localhost:8080/api/v1/event-hosts
+echo
