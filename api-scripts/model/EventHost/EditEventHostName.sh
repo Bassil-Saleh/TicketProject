7080c35f04e15d6cdf06fa019972552689867349
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-05 20:29:29 -0400

    Use named parameters for EditEventHostName.sh

--- api-scripts/model/EventHost/EditEventHostName.sh
+++ api-scripts/model/EventHost/EditEventHostName.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "Need to supply a JSON Web Token"
-	exit 1;
+# Defaults
+token=""
+firstName=""
+middleName=""
+lastName=""
+
+# Parse named parameters
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--token)       token="$2";       shift 2 ;;
+		--first-name)  firstName="$2";   shift 2 ;;
+		--middle-name) middleName="$2";  shift 2 ;;
+		--last-name)   lastName="$2";    shift 2 ;;
+		*)             shift ;;
+	esac
+done
+
+# Validate mandatory parameters
+if [ -z "$token" ] || [ -z "$firstName" ] || [ -z "$lastName" ]; then
+	echo "Usage: $0 --token <value> --first-name <value> --last-name <value> [--middle-name <value>]"
+	exit 1
 fi
 
-if [ -z "$2" ]; then
-	echo "Need to supply the new first name"
-	exit 1;
-fi
-
-if [ -z "$3" ]; then
-	echo "Need to supply the new middle name"
-	exit 1;
-fi
-
-if [ -z "$4" ]; then
-	echo "Need to supply the new last name"
-	exit 1;
-fi
-
-authHeader="Authorization: Bearer $1"
+authHeader="Authorization: Bearer $token"
 contentTypeHeader="Content-Type: application/json"
-json="{\"firstName\": \"$2\","
-json="${json} \"middleName\": \"$3\","
-json="${json} \"lastName\": \"$4\"}"
+json="{\"firstName\": \"$firstName\""
+if [ -n "$middleName" ]; then
+	json="${json}, \"middleName\": \"$middleName\""
+fi
+json="${json}, \"lastName\": \"$lastName\"}"
 
 curl -X PATCH -H "$authHeader" -H "$contentTypeHeader" -d "$json" http://localhost:8080/api/v1/event-hosts/name
+echo
