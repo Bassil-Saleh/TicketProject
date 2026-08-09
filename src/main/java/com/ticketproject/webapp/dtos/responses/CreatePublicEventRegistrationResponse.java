7ae9a708bdb9372e23268c66b47f16a2df16c58d
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-08 23:17:08 -0400

    Don't embed the ticket's public token value in the HTTP response anymore

--- src/main/java/com/ticketproject/webapp/dtos/responses/CreatePublicEventRegistrationResponse.java
+++ src/main/java/com/ticketproject/webapp/dtos/responses/CreatePublicEventRegistrationResponse.java
@@ -4,9 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 
 public record CreatePublicEventRegistrationResponse
 (
-    @NotBlank(message = "Public token of ticket cannot be blank")
-    String publicToken,
-
     @NotBlank(message = "Message cannot be blank")
     String message
 )
