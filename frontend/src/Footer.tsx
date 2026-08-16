16bdde233e2ee1fd7c5550a4fd03ae92b6f82e61
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-16 11:17:10 -0400

    Add styling to the footer

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -1,14 +1,12 @@
 /**
  * Footer is for implementing the UI which should always be
- * visible at the bottom of each page on the site. 
+ * visible at the bottom of each page on the site.
  * @returns JSX for the site's footer
  */
 export function Footer() {
-    // TODO: Add CSS styling.
-
     return (
-        <div>
+        <footer className="footer">
             <p>&copy; 2026 TicketProject. All rights reserved.</p>
-        </div>
+        </footer>
     );
 }
