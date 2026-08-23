3cc2b9f1e69a2fb08f8572dd6cc6863ec1601332
Bassil Saleh <bassilsaleh@posteo.com>
2026-08-22 23:05:53 -0400

    Add TODOs

--- frontend/src/CreateInvitationPage.tsx
+++ frontend/src/CreateInvitationPage.tsx
@@ -5,6 +5,25 @@
  * @returns JSX for the site's invitation creation page.
  */
 export function CreateInvitationPage() {
+    // TODO: If the user is not currently logged in, then automatically
+    //       redirect them to the home page.
+
+    // TODO: If the user is currently logged in, then display a form
+    //       to let them input:
+    //       - The invitee's first name.
+    //       - The invitee's middle name (optional).
+    //       - The invitee's last name.
+    //       - The invitee's email address.
+    //       - A "Send Invitation" button.
+    //       Also display a button to go back to the dashboard.
+
+    // TODO: When the user clicks "Send Invitation", send a request
+    //       to the API route POST /api/v1/attendees/invitation
+    //       using the form's input.
+
+    // TODO: Show an appropriate message to the user based on the
+    //       result of the sent request (success, failure, etc.).
+
     return (
         <div></div>
     );
--- frontend/src/InvitationResponsePage.tsx
+++ frontend/src/InvitationResponsePage.tsx
@@ -6,10 +6,20 @@
  * @returns JSX for the site's invitation response page.
  */
 export function InvitationResponsePage() {
-    // TODO: Extract the "publicToken" and "invitationResponse"
-    //       query parameters from the URL, put them in a JSON
-    //       request body, and send a request to the API route
-    //       PATCH
+    // TODO: Add a form that lets the user:
+    //       - Pick a response (Accepted or Rejected)
+    //       - Type an optional message to the event host
+    //         to include with their response
+
+    // TODO: Extract the "publicToken" URL query and include it
+    //       along with the optional message in a JSON request body
+    //       to the API route PATCH /api/v1/tickets/invitation.
+
+    // TODO: Show an appropriate message to the user based on the
+    //       result of the request (success, failure, etc.).
+
+    // TODO: If the "publicToken" URL query is missing, show an
+    //       error message to the user.
 
     return (
         <div></div>
